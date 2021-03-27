4e997528f58f76ed8ac3ae946558ba83ba48bd58
Alejandro Galue <agalue@opennms.org>
2021-03-27 17:17:06 -0400

    Fix protobuf code generation for flowdocument (not used yet).

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -3,7 +3,7 @@
 
 type protoc >/dev/null 2>&1 || { echo >&2 "protoc required but it's not installed; aborting."; exit 1; }
 
-for module in rpc sink telemetry netflow; do
+for module in rpc sink telemetry netflow flowdocument; do
   mkdir -p $module
   protoc --proto_path=./ --go_out=./ $module.proto
 done
